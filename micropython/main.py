1441d5fe8dfd14fbc2fb06a558de298d4b574822
jkp <100082388+KlimalaP@users.noreply.github.com>
2023-12-29 16:39:15 +0100

    Update main.py
    completion of basic functionality

--- micropython/main.py
+++ micropython/main.py
@@ -2,41 +2,64 @@ from machine import Pin, ADC
 from math import log
 from utime import sleep
 
-thermistor_pin = ADC(10)
-fan_pin = Pin(6, Pin.OUT)
-valve_pin = Pin(9, Pin.OUT)
+temp_sensor_pin = ADC(10)
+flame_detector_pin = Pin(2, Pin.IN)
+heater_pin = Pin(3, Pin.OUT)
+fan_pin = Pin(4, Pin.OUT)
+magneto_pin = Pin(5, Pin.OUT)
+valve_pin = Pin(6, Pin.OUT)
 ignition_pin = Pin(7, Pin.OUT)
-flame_detector_pin = Pin(4, Pin.IN)
-fault_of_initial_ignition = Pin(8, Pin.OUT)
-ignition_restart_button = Pin(13, Pin.OUT)
 
 blow_through = True
 restart_pressed = False
+working_temp_reached = False
+
+try_threshold = 4
+
 
 def adc_to_celsius(value_of_temp_from_adc):
-    return (1/(log(1/(65535/value_of_temp_from_adc-1))/3950 + 1/298.15))
+    return 1/(log(1/(65535/value_of_temp_from_adc-1))/3950 + 1/298.15)
 
 
-def restart_ignition(restart_button_pin):
-    ignition_restart_button.irq(trigger=Pin.IRQ_FALLING, handler=restart_ignition)
+def fan():
+    fan_pin.value(1)
+    sleep(5)
+    fan_pin.value(0)
 
-#write def to ignition
+
+def magneto():
+    magneto_pin.value(1)
+    sleep(6)
+    magneto_pin.value(0)
+
+
+def temperature_check(term_val):
+    if thermistor_value < 90:
+        heater_pin.value(1)
+    else:
+        heater_pin.value(0)
+
 
 while True:
-    thermistor_value = thermistor_pin.read_u16()
+    thermistor_value = temp_sensor_pin.read_u16()
     thermistor_value = adc_to_celsius(thermistor_value)
-    if thermistor_value == 90 & blow_through == blow_through:
-        fan_pin.value(1)
-        sleep(5)
-        fan_pin.value(0)
-    else:
-        valve_pin.value(1)
-        ignition_pin.value(1)
-        sleep(6)
-        if flame_detector_pin.value() == 1:
-            blow_through = False
-        else:
-            fault_of_initial_ignition.value(1)
-            while not restart_pressed: #false by default
-                sleep(0.1)
 
+    while not working_temp_reached:
+        temperature_check(thermistor_value)
+    else:
+        working_temp_reached = True
+
+    if thermistor_value == 90 & blow_through == blow_through: #initialy blow_through is set to True
+        fan()
+        valve_pin.value(1)
+        magneto()
+        if flame_detector_pin.value == 0:
+            for _ in range(try_threshold):
+                fan()
+                magneto_pin()
+                if flame_detector_pin == 1:
+                    valve_pin.value(0)
+                    break
+        elif flame_detector_pin.value == 0 & valve_pin.value() == 1:
+            valve_pin.value(0)
+            raise ValueError(f"Failed ignition.")
