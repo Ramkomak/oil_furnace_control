8e50f6c54b54a11a27711af724f8a1c85ba1bcf0
Ramkomak <101971146+Ramkomak@users.noreply.github.com>
2024-01-03 22:54:28 +0100

    base functionality

--- micropython/main.py
+++ micropython/main.py
@@ -1,93 +1,78 @@
-from machine import Pin, ADC
-from math import log
-from utime import sleep
-import bluetooth
+from machine import Pin, I2C
+from time import sleep_ms
+import onewire
+import ds18x20
+import ssd1306
 
-temp_sensor_pin = ADC(10)
-flame_detector_pin = Pin(2, Pin.IN)
+working_flag = False
+oil_temp = 30
+error_count = 0
+
+pre_purge_time = 5000  # ms
+igniter_time = 6000  # ms
+
+flame_detector_pin = Pin(2, Pin.IN, Pin.PULL_UP)
+valve_pin = Pin(2, Pin.OUT)
 heater_pin = Pin(3, Pin.OUT)
 fan_pin = Pin(4, Pin.OUT)
 magneto_pin = Pin(5, Pin.OUT)
-valve_pin = Pin(6, Pin.OUT)
-ignition_pin = Pin(7, Pin.OUT)
+valve_pin.value(1)
+heater_pin.value(1)
+fan_pin.value(1)
+magneto_pin.value(1)
 
-blow_through = True
-restart_pressed = False
-working_temp_reached = False
+OK_button = Pin(10, Pin.IN, Pin.PULL_UP)
+UP_button = Pin(11, Pin.IN, Pin.PULL_UP)
+DOWN_button = Pin(12, Pin.IN, Pin.PULL_UP)
 
-try_threshold = 4
+i2c = I2C(0, sda=Pin(8), scl=Pin(9))
+display = ssd1306.SSD1306_I2C(128, 64, i2c)
+
+temp_sensor = ds18x20.DS18X20(onewire.OneWire(Pin(7)))
+temp = temp_sensor.scan()
 
 
-def adc_to_celsius(value_of_temp_from_adc):
-    return 1/(log(1/(65535/value_of_temp_from_adc-1))/3950 + 1/298.15)
+def check_temp():
+    temp_sensor.convert_temp()
+    sleep_ms(750)
+    for rom in temp:
+        return temp_sensor.read_temp(rom)
 
 
-def fan():
-    fan_pin.value(1)
-    sleep(5)
+def state_machine(err):
     fan_pin.value(0)
-
-
-def magneto():
-    magneto_pin.value(1)
-    sleep(6)
+    sleep_ms(pre_purge_time)
+    valve_pin.value(0)
+    sleep_ms(100)
     magneto_pin.value(0)
-
-
-def temperature_check(term_val):
-    if thermistor_value < 90:
-        heater_pin.value(1)
+    sleep_ms(igniter_time)
+    magneto_pin.value(1)
+    if flame_detector_pin == 0:
+        return True
     else:
-        heater_pin.value(0)
+        valve_pin.value(1)
+        err += 1
+        return False
+def clear_display():
+    display.fill(0)
+    display.show()
+def show_on_display(txt):
+    display.fill(0)
+    display.text(txt, 0, 0, 2)
+    display.show()
 
 
-def send_data(data):
-    target_address = "XX:XX:XX:XX:XX:XX"
-
-    port = 1
-
-    try:
-        sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-        sock.connect((target_address, port))
-
-        sock.send(data)
-
-        print(data)
-
-        sock.close()
-
-    except Exception as e:
-        print(f"Error: {e}")
 
 
 while True:
-    thermistor_value = temp_sensor_pin.read_u16()
-    thermistor_value = adc_to_celsius(thermistor_value)
-
-    while not working_temp_reached:
-        temperature_check(thermistor_value)
+    print("dupa")
+    if check_temp() >= oil_temp:
+        show_on_display('Temperature Reached!')
+        state_machine(error_count)
     else:
-        working_temp_reached = True
-
-    if thermistor_value == 90 & blow_through == blow_through: #initialy blow_through is set to True
-        fan()
-        valve_pin.value(1)
-        magneto()
-        if flame_detector_pin.value == 0:
-            for _ in range(try_threshold):
-                fan()
-                magneto_pin()
-                if flame_detector_pin == 1:
-                    valve_pin.value(0)
-                    break
-        elif flame_detector_pin.value == 0 & valve_pin.value() == 1:
-            valve_pin.value(0)
-            raise ValueError(f"Failed ignition.")
-
-
-
-
-
+        show_on_display('Low Temperature!')
+        sleep_ms(100)
+        clear_display()
 
 
 
