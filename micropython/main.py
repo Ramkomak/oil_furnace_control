0cb75fd6992a2f6cca28205cd45a21989a8501be
Ramkomak <101971146+Ramkomak@users.noreply.github.com>
2024-01-05 21:31:35 +0100

    big update

--- micropython/main.py
+++ micropython/main.py
@@ -1,25 +1,28 @@
-from machine import Pin, I2C
-from time import sleep_ms
+from machine import Pin, I2C, Timer
+from time import sleep_ms,
 import onewire
 import ds18x20
 import ssd1306
 
+tim = Timer()
+start_tim = Timer()
+flame_watch = Timer()
+oil_watch = Timer()
 working_flag = False
+Oil_good = False
+curr_state = 1
 oil_temp = 30
+hysteresis_width = 1
 error_count = 0
 
 pre_purge_time = 5000  # ms
 igniter_time = 6000  # ms
 
 flame_detector_pin = Pin(2, Pin.IN, Pin.PULL_UP)
-valve_pin = Pin(2, Pin.OUT)
-heater_pin = Pin(3, Pin.OUT)
-fan_pin = Pin(4, Pin.OUT)
-magneto_pin = Pin(5, Pin.OUT)
-valve_pin.value(1)
-heater_pin.value(1)
-fan_pin.value(1)
-magneto_pin.value(1)
+valve_pin = Pin(2, Pin.OUT, value=1)
+heater_pin = Pin(3, Pin.OUT, value=1)
+fan_pin = Pin(4, Pin.OUT, value=1)
+magneto_pin = Pin(5, Pin.OUT, value=1)
 
 OK_button = Pin(10, Pin.IN, Pin.PULL_UP)
 UP_button = Pin(11, Pin.IN, Pin.PULL_UP)
@@ -32,47 +35,140 @@ temp_sensor = ds18x20.DS18X20(onewire.OneWire(Pin(7)))
 temp = temp_sensor.scan()
 
 
+def start_procedure(Source):
+    print("test")
+    if Oil_good and working_flag == False:
+        real_state_machine()
+    if working_flag and flame_detector_pin.value() == 0:
+        pass
+    else:
+        display.fill(0)
+        display.text("AWARIA!!!",0,32,1)
+        display.show()
+        valve_pin.value(1)
+        fan_pin.value(1)
+
+
+
 def check_temp():
     temp_sensor.convert_temp()
-    sleep_ms(750)
     for rom in temp:
         return temp_sensor.read_temp(rom)
 
+def next_state(Source):
+    global curr_state
+    curr_state += 1
+    real_state_machine()
 
-def state_machine(err):
-    fan_pin.value(0)
-    sleep_ms(pre_purge_time)
-    valve_pin.value(0)
-    sleep_ms(100)
-    magneto_pin.value(0)
-    sleep_ms(igniter_time)
-    magneto_pin.value(1)
-    if flame_detector_pin == 0:
-        return True
-    else:
-        valve_pin.value(1)
-        err += 1
-        return False
+def real_state_machine():
+    global curr_state, working_flag, error_count
+
+    if curr_state == 1:
+        print("state1")
+        fan_pin.value(0)
+        start_tim.deinit()
+        tim.init(mode=Timer.ONE_SHOT, period=5000, callback=next_state)
+    if curr_state == 2:
+        print("state2")
+        valve_pin.value(0)
+        tim.init(mode=Timer.ONE_SHOT, period=500, callback=next_state)
+    if curr_state == 3:
+        print("state3")
+        magneto_pin.value(0)
+        tim.init(mode=Timer.ONE_SHOT, period=6000, callback=next_state)
+    if curr_state == 4:
+        print("state4")
+        magneto_pin.value(1)
+        if flame_detector_pin.value() == 0:
+            working_flag = True
+            flame_watch.init(mode=Timer.PERIODIC, period=1000, callback=start_procedure)
+            print("odpalony")
+        else:
+            valve_pin.value(1)
+            error_count += 1
+            working_flag = False
+            curr_state = 1
+            if error_count < 5 and Oil_good:
+                real_state_machine()
+            else:
+                pass
+
+
 def clear_display():
     display.fill(0)
     display.show()
+
+
 def show_on_display(txt):
     display.fill(0)
-    display.text(txt, 0, 0, 2)
+    display.text(txt, 0, 0, 1)
     display.show()
+
+
+def which_button():
+    if OK_button.value() == 0:
+        sleep_ms(20)
+        return 1
+    if UP_button.value() == 0:
+        sleep_ms(20)
+        return 2
+    if DOWN_button.value() == 0:
+        sleep_ms(20)
+        return 3
+    else:
+        return 0
+
+
+def oil_temp_check(Source):
+    global Oil_good
+    if check_temp() <= oil_temp - hysteresis_width:
+        heater_pin.value(0)
+        Oil_good = False
+
+    if check_temp() > oil_temp:
+        Oil_good = True
+
+    if check_temp() >= oil_temp + hysteresis_width:
+        heater_pin.value(1)
+
+
+print(flame_detector_pin.value())
+start_tim.init(mode=Timer.PERIODIC, period=1000, callback=start_procedure)
+oil_watch.init(mode=Timer.PERIODIC, period=1000, callback=oil_temp_check)
 
 
 
 
 while True:
-    print("dupa")
-    if check_temp() >= oil_temp:
+    # oil_temp_check()
+
+    if heater_pin.value() == 0:
+        display.text("Heater ON", 0, 32, 1)
+
+    if which_button() == 1:
+        print("button active")
+        clear_display()
+        display.text("Actual oil temp:", 0, 0, 1)
+        display.text(str(check_temp()), 0, 16, 1)
+        display.text("Temp set:", 0, 32, 1)
+        display.text(str(oil_temp), 74, 32, 1)
+        display.show()
+        while True:
+            sleep_ms(100)
+            if which_button() > 1:
+                break
+            else:
+                continue
+
+    if Oil_good:
         show_on_display('Temperature Reached!')
-        state_machine(error_count)
+        # real_state_machine()
+        # working_flag = state_machine(error_count)
     else:
         show_on_display('Low Temperature!')
         sleep_ms(100)
         clear_display()
+
 
 
 
